bf59fd6b3a1db05209f2edd6dadb423c027a8fa2
Alvin Lee <ALEE9@PROTONMAIL.COM>
2019-04-09 15:45:31 +0900

    pretty sure these are not correct. please advise

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!({
+User.create!(
   name: 'Alvin',
   email: 'alvin@alvin.com',
   password: 'aaaaaa'
-})
+)
+
+Ingredient.create!(
+  name: 'chicken'
+)
+
+Dose.create!(
+  value: 0
+  unit: 0
+)
+
+Meal.create!(
+  title: 'Tuesday dinner'
+  image_url: 'https://baconmockup.com/640/360'
+)
 
