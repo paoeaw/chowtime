bffe3d840bfdb954e0c609d195579a35fbbba48a
Alexander Moskowitz <alexjmoskowitz@gmail.com>
2019-04-09 15:54:52 +0900

    defined two methods that perform spoonaculars api requests

--- app/controllers/meal_plans_controller.rb
+++ app/controllers/meal_plans_controller.rb
@@ -15,4 +15,38 @@ class MealPlansController < ApplicationController
 
   def create
   end
+
+  private
+
+  # returns an array of recipe ids from Spoonacular
+  def obtain_recipe_ids(calories, diet_type, excluded_ingredients)
+    recipe_ids = []
+    conn = Faraday.new(url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=week&targetCalories=#{calories}&diet=#{diet_type}&exclude=#{excluded_ingredients}")
+    conn.headers["X-RapidAPI-Host"] = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+    conn.headers["X-RapidAPI-Key"] = "ef1d7556fdmshc6fc5f15f724b5ap16c498jsn525dd13a636e"
+    response = conn.get
+    response_data = JSON.parse(response.body)
+    response_data['items'].each do |item|
+      recipe_hash = JSON.parse(item['value'])
+      recipe_ids << recipe_hash['id']
+    end
+    recipe_ids
+  end
+
+  # returns a hash of ingredient names/quantities from Spoonacular
+  # requires an array of recipe ids
+  def ingredients(recipe_ids)
+    ingredients = {}
+    recipe_ids.each do |id|
+      conn = Faraday.new(url: "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/#{id}/ingredientWidget.json")
+      conn.headers["X-RapidAPI-Host"] = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+      conn.headers["X-RapidAPI-Key"] = "ef1d7556fdmshc6fc5f15f724b5ap16c498jsn525dd13a636e"
+      response = conn.get
+      response_data = JSON.parse(response.body)
+      response_data['ingredients'].each do |ingredient|
+        ingredients[ingredient['name']] = ingredient['amount']
+      end
+    end
+    ingredients
+  end
 end
