d5fae63757adc6683f28f3a681c7f5a8ef39780b
Alexander Moskowitz <alexjmoskowitz@gmail.com>
2019-04-11 16:33:38 +0900

    testing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ import { removeFryingPan } from '../components/loading-pan';
 
 selectIngredients();
 
-const pans = document.querySelectorAll('.loading-pan');
-if(pans[0]) {
-  setInterval ( "removeFryingPan()", 3000 );
-}
+// const pans = document.querySelectorAll('.loading-pan');
+// if(pans[0]) {
+//   setInterval ( "removeFryingPan()", 3000 );
+// }
