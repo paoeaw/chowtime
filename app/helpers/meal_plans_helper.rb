841206afbfaf17f709ae09140bf804159621311a
Alexander Moskowitz <alexjmoskowitz@gmail.com>
2019-04-12 01:20:41 +0900

    api calls now work correctly

--- app/helpers/meal_plans_helper.rb
+++ app/helpers/meal_plans_helper.rb
@@ -27,11 +27,14 @@ module MealPlansHelper
       meal.meal_plan = meal_plan
       meal.save
       response_data['extendedIngredients'].each do |ext_ingr|
-        ingredient = Ingredient.find_or_create_by(name: ext_ingr['name'], aisle: ext_ingr['aisle'])
-        dose = Dose.new(value: ext_ingr['amount'], unit: ext_ingr['unit'], direction: ext_ingr['originalString'])
-        dose.ingredient = ingredient
-        dose.meal = meal
-        dose.save
+        unless ext_ingr['amount'].zero?
+          ingredient = Ingredient.create(name: ext_ingr['name'], aisle: ext_ingr['aisle'])
+          dose = Dose.new(value: ext_ingr['amount'], unit: ext_ingr['unit'], direction: ext_ingr['originalString'])
+          dose.ingredient = ingredient
+          dose.meal_plan = meal_plan
+          dose.meal = meal
+          dose.save
+        end
       end
     end
   end
