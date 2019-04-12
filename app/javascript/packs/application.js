259d518835c6ba33bce857b00904982c3b296b05
Paphada Eawsivigoon <paphadaeawsivigoon@gmail.com>
2019-04-12 14:35:30 +0900

    diet type use icon instead form and hide all form fields

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import "bootstrap";
 
 import { selectIngredients } from '../components/selectingredients';
 import { selectKcal } from '../components/selectkcal';
+import { selectDietType } from '../components/selecttype';
 
 selectIngredients();
 selectKcal();
+selectDietType();
