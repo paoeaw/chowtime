d9d234c81cbc791f76b4832fb6ef6a6d5f57810f
Paphada Eawsivigoon <paphadaeawsivigoon@gmail.com>
2019-04-12 12:58:22 +0900

    clicked icon identification

--- app/javascript/components/selectkcal.js
+++ app/javascript/components/selectkcal.js
@@ -3,6 +3,7 @@ const selectKcal = () => {
 
   kcal.forEach((item) => {
     item.addEventListener('click', (event) => {
+      event.currentTarget.classList.toggle('active');
       const caloriesSearch = document.getElementById('meal_params_calories');
       const calories = item.querySelector('#calories');
       caloriesSearch.value = calories.innerHTML;
