69b289db0d62a9289bcd4a35efeb15628f8b864d
Paphada Eawsivigoon <paphadaeawsivigoon@gmail.com>
2019-04-12 17:04:47 +0900

    remove active class when click another icon

--- app/javascript/components/selectkcal.js
+++ app/javascript/components/selectkcal.js
@@ -3,7 +3,8 @@ const selectKcal = () => {
 
   kcal.forEach((item) => {
     item.addEventListener('click', (event) => {
-      event.currentTarget.classList.toggle('active');
+      kcal.forEach((item) => item.classList.remove('active'));
+      event.currentTarget.classList.add('active');
       const caloriesSearch = document.getElementById('meal_params_calories');
       const calories = item.querySelector('#calories');
       caloriesSearch.value = calories.innerHTML;
