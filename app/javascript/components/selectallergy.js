d2bd2072bc2ed417a20b1c19ea70dd3a2af7cae0
Paphada Eawsivigoon <paphadaeawsivigoon@gmail.com>
2019-04-15 11:40:25 +0900

    click icon to add or remove exclusions

---
+++ app/javascript/components/selectallergy.js
@@ -0,0 +1,21 @@
+const selectAllergy = () => {
+  const exclusion = document.querySelectorAll('.exclusion');
+  const array = [];
+
+  exclusion.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      event.currentTarget.classList.toggle('active');
+      const exclusionSearch = document.getElementById('meal_params_exclusions');
+      const allergy = item.querySelector('#allergy').innerHTML;
+      if (array.includes(allergy)) {
+        array.splice(array.indexOf(allergy), 1);
+      }
+      else {
+        array.push(allergy);
+      };
+      exclusionSearch.value = array.join(',');
+    });
+  });
+}
+
+export { selectAllergy };
