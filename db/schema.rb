30b45fdaf6281a140177a7a06028950ba11f3ed2
Alexander Moskowitz <alexjmoskowitz@gmail.com>
2019-04-17 15:02:22 +0900

    meal plans are populated with personalized diet info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_17_045921) do
+ActiveRecord::Schema.define(version: 2019_04_17_054734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2019_04_17_045921) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "diet_type"
+    t.string "exclusions"
+    t.string "calories"
     t.index ["user_id"], name: "index_meal_plans_on_user_id"
   end
 
