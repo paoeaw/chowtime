f88cf7ee272daba51eb9a3dee718517f4d9700ea
Alexander Moskowitz <alexjmoskowitz@gmail.com>
2019-04-18 01:00:42 +0900

    more styling, javascript

--- app/javascript/components/loading-pan.js
+++ app/javascript/components/loading-pan.js
@@ -2,9 +2,13 @@
   const removeFryingPan = () => {
     let pans = document.querySelectorAll('.loading-pan');
     let doses = document.querySelectorAll('.dose');
+    let onLoadElements = document.querySelectorAll('.appear-on-load')
     if(doses.length > 0) {
       pans.forEach((pan) => {
         pan.style.display = 'none'
+      });
+      onLoadElements.forEach((element) => {
+        element.classList.remove('d-none');
       });
     } else {
       console.log('no ingredients yet')
